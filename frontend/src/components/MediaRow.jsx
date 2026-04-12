90c37d822a16b9056d52f4ee6f664b11d3e45a1d
AaroHaavisto <118162246+AaroHaavisto@users.noreply.github.com>
2026-04-12 11:05:37 +0300

    Implement selected item state with SingleView dialog

--- frontend/src/components/MediaRow.jsx
+++ frontend/src/components/MediaRow.jsx
@@ -1,9 +1,21 @@
-const MediaItem = ({item}) => {
-  console.log('item', item);
+import PropTypes from 'prop-types';
+
+const MediaRow = ({item, selectedItem, setSelectedItem}) => {
+  const isSelected = selectedItem?.media_id === item.media_id;
+
   return (
-    <tr key={item.filename}>
+    <tr className={isSelected ? 'media-row media-row-selected' : 'media-row'}>
       <td>
-        <img src={item.thumbnail} />
+        <button
+          type="button"
+          className="open-single-view-btn"
+          onClick={() => {
+            setSelectedItem(item);
+          }}
+        >
+          Open
+        </button>
+        <img src={item.thumbnail} alt={item.title} />
       </td>
       <td>{item.title}</td>
       <td>{item.description}</td>
@@ -14,4 +26,25 @@ const MediaItem = ({item}) => {
   );
 };
 
-export default MediaItem;
+MediaRow.propTypes = {
+  item: PropTypes.shape({
+    media_id: PropTypes.number.isRequired,
+    filename: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    created_at: PropTypes.string.isRequired,
+    filesize: PropTypes.number.isRequired,
+    media_type: PropTypes.string.isRequired,
+  }).isRequired,
+  selectedItem: PropTypes.shape({
+    media_id: PropTypes.number,
+  }),
+  setSelectedItem: PropTypes.func.isRequired,
+};
+
+MediaRow.defaultProps = {
+  selectedItem: null,
+};
+
+export default MediaRow;
